fba88fc03a41e91397b1c9c62ea2abec1ea946c2
Doğukan Demir <senseizeus190701@gmail.com>
2025-03-22 14:07:01 +0000

    feat: Add element retrieval script

---
+++ element.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit 0
+fi
+
+ELEMENT_QUERY=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number='$1' OR symbol='$1' OR name='$1'")
+
+if [[ -z $ELEMENT_QUERY ]]
+then
+  echo "I could not find that element in the database."
+else
+  IFS="|" read -r ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT <<< "$ELEMENT_QUERY"
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+fi
